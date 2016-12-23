e0b7b3eb74983e291778456218a33c059d9d2711
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-12-22 22:51:05 -0600

    checkpoint 2

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
  # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
- gem 'rails', '4.2.5'
+ gem 'rails', '5.0.0.1'
 
  group :production do
    gem 'pg'
@@ -10,16 +10,19 @@ source 'https://rubygems.org'
 
  group :development do
    gem 'sqlite3'
+   gem 'listen', '~> 3.0.5'
  end
 
+ gem 'bootstrap-sass'
  gem 'sass-rails', '~> 5.0'
  gem 'uglifier', '>= 1.3.0'
  gem 'coffee-rails', '~> 4.1.0'
  gem 'jquery-rails'
  gem 'turbolinks'
- gem 'bootstrap'
+ gem 'bootstrap', '~> 4.0.0.alpha4'
  gem 'figaro'
  gem 'pry'
+ gem 'devise'
 
  group :development, :test do
    gem 'byebug'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -26,6 +28,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -50,5 +54,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
