27270047c12ff7c5b9da7d783ad12e03adb4d494
Allen Peterson <alpeterson@Als-MacBook-Pro.local>
2016-12-28 20:01:45 -0600

    checkpoint_4

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,10 +4,18 @@ class WikisController < ApplicationController
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
   end
 
   def create
-    @wiki = Wiki.create(wiki_params)
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.user = current_user
 
     if @wiki.save
       flash[:notice] = "Wiki was saved."
@@ -15,17 +23,41 @@ class WikisController < ApplicationController
     else
       flash.now[:alert] = "There was an error creating your wiki. Please try again."
       render :new
+    end
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
 
   def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+
+    if @wiki.save
+        flash[:notice] = "Wiki was updated"
+        redirect_to @wiki
+    else
+        flash.now[:alert] = "Error saving wiki. Please try again."
+        render :edit
+    end
   end
 
   def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+        flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+        redirect_to wikis_path
+    else
+        flash.now[:alert] = "There was an error deleting the wiki"
+        render :show
+    end
   end
+
+  private
 
   def wiki_params
     params.require(:wiki).permit(:title, :body, :private)
+  end
 end
